1facef7ef46152d69e793df941f9a83043f5e7c3
knishihama <47240829+knishihama@users.noreply.github.com>
2019-02-27 21:57:59 +0900

    一旦戻し

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const {Nuxt} = require('nuxt')
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || '3000'
+// Import and set Nuxt.js options
 let config = require('./nuxt.config.js')
 const nuxt = new Nuxt(config)
-process.argv[2] = 'start' // set argument to 'start'
-require('nuxt/bin/nuxt') // execute command 'nuxt
+// Give Nuxt middleware to express
+app.use(nuxt.render)
+// Listen the server
+app.listen(port, host);
+console.log('baseUrl :' + baseUrl);
+console.log('Server listening on ' + host + ':' + port);
