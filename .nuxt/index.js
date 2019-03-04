1285a95799bdda8b4a0de2b218bd4c062da678eb
knishihama <47240829+knishihama@users.noreply.github.com>
2019-03-04 22:56:13 +0900

    ご利用規定のリンク修正

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,9 +12,7 @@ import { createStore } from './store.js'
 
 /* Plugins */
 
-import nuxt_plugin_swplugin_60452afe from 'nuxt_plugin_swplugin_60452afe' // Source: ./sw.plugin.js (ssr: false)
-import nuxt_plugin_axios_c6f04036 from 'nuxt_plugin_axios_c6f04036' // Source: ./axios.js
-import nuxt_plugin_axios_5659cf26 from 'nuxt_plugin_axios_5659cf26' // Source: ..\\plugins\\axios.ts
+import nuxt_plugin_axios_79e4f376 from 'nuxt_plugin_axios_79e4f376' // Source: ./axios.js
 
 // Component: <no-ssr>
 Vue.component(NoSSR.name, NoSSR)
@@ -153,12 +151,7 @@ async function createApp(ssrContext) {
 
   // Plugin execution
 
-  if (typeof nuxt_plugin_axios_c6f04036 === 'function') await nuxt_plugin_axios_c6f04036(app.context, inject)
-  if (typeof nuxt_plugin_axios_5659cf26 === 'function') await nuxt_plugin_axios_5659cf26(app.context, inject)
-
-  if (process.client) {
-    if (typeof nuxt_plugin_swplugin_60452afe === 'function') await nuxt_plugin_swplugin_60452afe(app.context, inject)
-  }
+  if (typeof nuxt_plugin_axios_79e4f376 === 'function') await nuxt_plugin_axios_79e4f376(app.context, inject)
 
   // If server-side, wait for async component to be resolved first
   if (process.server && ssrContext && ssrContext.url) {
